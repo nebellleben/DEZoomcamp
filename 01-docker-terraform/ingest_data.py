ab83203f7cfc66006d9df6db363672f07808f4ec
Kelvin Chan <kelvinchan@Kelvins-MacBook-Air.local>
2026-01-17 14:59:51 +0800

    Homework 1 - after adding click

--- 01-docker-terraform/ingest_data.py
+++ 01-docker-terraform/ingest_data.py
@@ -1,71 +1,58 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import click
+# ## Test Injection of Data
+# 
+# - This note book try to import parquet file into postgreSQL database
+# - Remember to implement the click part to take in command line parameters
+# - When done it will be converted to script using nbconvert
+
+# In[1]:
+
+
 import pandas as pd
+import click
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import pyarrow.parquet as pq
 
-dtype = {
-    "VendorID": "Int64",
-    "passenger_count": "Int64",
-    "trip_distance": "float64",
-    "RatecodeID": "Int64",
-    "store_and_fwd_flag": "string",
-    "PULocationID": "Int64",
-    "DOLocationID": "Int64",
-    "payment_type": "Int64",
-    "fare_amount": "float64",
-    "extra": "float64",
-    "mta_tax": "float64",
-    "tip_amount": "float64",
-    "tolls_amount": "float64",
-    "improvement_surcharge": "float64",
-    "total_amount": "float64",
-    "congestion_surcharge": "float64"
-}
 
-parse_dates = [
-    "tpep_pickup_datetime",
-    "tpep_dropoff_datetime"
-]
-
+#pg_user = 'postgres'
+#pg_pass = 'postgres'
+#pg_host = 'localhost'
+#pg_port = '5433'
+#pg_db = 'ny_taxi'
+#target_table = 'green_taxi_trips'
 
 @click.command()
 @click.option('--pg-user', default='postgres', help='PostgreSQL user')
 @click.option('--pg-pass', default='postgres', help='PostgreSQL password')
-@click.option('--pg-host', default='db', help='PostgreSQL host')
-@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5433, type=int, help='PostgreSQL port')
 @click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
-@click.option('--year', default=2021, type=int, help='Year of the data')
-@click.option('--month', default=1, type=int, help='Month of the data')
-@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
-@click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
-def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
-    """Ingest NYC taxi data into PostgreSQL database."""
-    #prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
-    # url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
-    url = './green_tripdata_2025-11.parquet'
-
+#@click.option('--year', default=2021, type=int, help='Year of the data')
+#@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--target-table', default='green_taxi_trips', help='Target table name')
+#@click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table):
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
-    df_iter = pd.read_csv(
-        url,
-        dtype=dtype,
-        parse_dates=parse_dates,
-        iterator=True,
-        chunksize=chunksize,
-    )
-
     first = True
+    preview_df = None
 
-    for df_chunk in tqdm(df_iter):
+    parquet_path = "./green_tripdata_2025-11.parquet"
+    parquet_file = pq.ParquetFile(parquet_path)
+
+    for batch in tqdm(parquet_file.iter_batches(batch_size=100000)):
+        df_chunk = batch.to_pandas()
+
         if first:
             df_chunk.head(0).to_sql(
                 name=target_table,
                 con=engine,
                 if_exists='replace'
             )
+            preview_df = df_chunk.head()
             first = False
 
         df_chunk.to_sql(
@@ -74,5 +61,11 @@ def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, ch
             if_exists='append'
         )
 
+    if preview_df is not None:
+        print(preview_df)
+
+
+
 if __name__ == '__main__':
     run()
+
