a7c7569d045c4c1a755f5bbb29119d8e902ef9e6
DEZoomcamp Analytics Engineer <dezoomcamp@example.com>
2026-02-25 21:54:59 +0800

    Homework 5

--- 05-data-platforms/my-pipeline/pipeline/assets/ingestion/trips.py
+++ 05-data-platforms/my-pipeline/pipeline/assets/ingestion/trips.py
@@ -7,6 +7,8 @@ materialization:
   type: table
   strategy: append
 
+connection: duckdb-default
+
 columns:
   - name: pickup_datetime
     type: timestamp
@@ -20,14 +22,32 @@ import os
 import json
 import pandas as pd
 
+
 def materialize():
     start_date = os.environ["BRUIN_START_DATE"]
     end_date = os.environ["BRUIN_END_DATE"]
     taxi_types = json.loads(os.environ["BRUIN_VARS"]).get("taxi_types", ["yellow"])
 
-    # Generate list of months between start and end dates
-    # Fetch parquet files from:
-    # https://d37ci6vzurychx.cloudfront.net/trip-data/{taxi_type}_tripdata_{year}-{month}.parquet
+    from datetime import datetime
+    from dateutil.relativedelta import relativedelta
+
+    start = datetime.fromisoformat(start_date.replace("Z", "+00:00"))
+    end = datetime.fromisoformat(end_date.replace("Z", "+00:00"))
+
+    months = []
+    current = start.replace(day=1)
+    while current <= end:
+        months.append((current.year, current.month))
+        current += relativedelta(months=1)
+
+    dfs = []
+    for taxi_type in taxi_types:
+        for year, month in months:
+            url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{taxi_type}_tripdata_{year}-{month:02d}.parquet"
+            df = pd.read_parquet(url)
+            df["taxi_type"] = taxi_type
+            dfs.append(df)
+
+    final_dataframe = pd.concat(dfs, ignore_index=True) if dfs else pd.DataFrame()
 
     return final_dataframe
-
